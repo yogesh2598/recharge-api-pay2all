d26982c6fc942d649615e2b5ab2454afa6363c2d
yogesh <31534940+yogesh2598@users.noreply.github.com>
2017-11-26 17:39:16 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,35 @@
+<?php
+//sample php code
+
+//this will collect data from form
+$provider_id = $_POST['provider_id'];
+$number = $_POST['number'];
+$amount = $_POST['amount'];
+$client_id = "09919190"; //(your system unique id. that you can check in pay2all);
+//end of data collection from form
+
+
+//check whether user enter some data or not
+        if(empty($provider_id)){
+        echo"select operator";
+        exit;
+        }
+        if(empty($number)){
+        echo"enter mobile number";
+        exit;
+        }
+        if(empty($amount)){
+        echo"enter amount";
+        exit;
+        }
+        $api_token = "bcMI70jWyplO5fuCyfzJYmF5qJOzn06tJemJ9vxSqa5sKcU4faPo4hmqm4HY"; // api_token token will in (https://www.pay2all.in/developers/recharge-api-doc)
+        $ch = curl_init();
+        $url = "https://www.pay2all.in/web-api/paynow?api_token=$api_token&number=$number&provider_id=$provider_id&amount=$amount&client_id=$client_id";
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        echo $response;
+
+
+?>
